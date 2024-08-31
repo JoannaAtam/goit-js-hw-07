e75c490d7287ba4aa781527a8136582c801cf622
JoannaAtam <joanna.atamanczuk@gmail.com>
2024-08-31 14:50:14 +0200

    map

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,26 @@
-const productById = document.querySelector("#categories");
-const productNameByTag = productById.querySelectorAll(".item");
+//const productById = document.querySelector("#categories");
+//const productNameByTag = productById.querySelectorAll(".item");
 
-console.log("Number of categories: " + productNameByTag.length);
+///console.log("Number of categories: " + productNameByTag.length);
 
-productNameByTag.forEach(item => {
-    const categoryName = item.querySelector("h2").innerHTML;
-    const elementsCount = item.querySelectorAll("li").length;
-    
-    console.log("Category: " + categoryName);
-    console.log("Elements: " + elementsCount);
+//productNameByTag.forEach(item => {
+    //const categoryName = item.querySelector("h2").innerHTML;
+    //const elementsCount = item.querySelectorAll("li").length;
+    //console.log("Category: " + categoryName);
+    //console.log("Elements: " + elementsCount);
+//});
+
+const categories = document.querySelectorAll("#categories .item");
+
+console.log("Number of categories: " + categories.length);
+
+const categoryDetails = Array.from(categories).map(category => {
+    const title = category.querySelector("h2").textContent;
+    const elementsCount = category.querySelectorAll("ul li").length;
+    return { title, elementsCount };
 });
 
-
-
-
-
+categoryDetails.forEach(detail => {
+    console.log("Category: " + detail.title);
+    console.log("Elements: " + detail.elementsCount);
+});
