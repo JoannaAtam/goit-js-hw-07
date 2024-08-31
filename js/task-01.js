9708e6d4f70ffba3a15f23321f34b3159dee58dc
JoannaAtam <joanna.atamanczuk@gmail.com>
2024-08-31 14:52:21 +0200

    js1 właściwy

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,3 @@
-//const productById = document.querySelector("#categories");
-//const productNameByTag = productById.querySelectorAll(".item");
-
-///console.log("Number of categories: " + productNameByTag.length);
-
-//productNameByTag.forEach(item => {
-    //const categoryName = item.querySelector("h2").innerHTML;
-    //const elementsCount = item.querySelectorAll("li").length;
-    //console.log("Category: " + categoryName);
-    //console.log("Elements: " + elementsCount);
-//});
-
 const categories = document.querySelectorAll("#categories .item");
 
 console.log("Number of categories: " + categories.length);
