1dfcbe2d6b1ef7163062a26c927974894ed6308a
JoannaAtam <joanna.atamanczuk@gmail.com>
2024-08-31 14:45:11 +0200

    Pętla forEach

--- js/task-01.js
+++ js/task-01.js
@@ -1,32 +1,15 @@
-//czesc 1 - Policzy i wyświetli w konsoli liczbę kategorii w ul#categories, czyli elementów li.item.
+const productById = document.querySelector("#categories");
+const productNameByTag = productById.querySelectorAll(".item");
 
-const ProductById = document.querySelector("#categories");
-//console.log(ProductById);
+console.log("Number of categories: " + productNameByTag.length);
 
-const ProductNameByTag = ProductById.querySelectorAll(".item");
-
-console.log("Number of categories:" + ProductNameByTag.length);
-
-//czesc 2 - Dla każdego elementu li.item na liście ul#categories, 
-//znajdzie i wyświetli w konsoli tekst tytułu elementu (tagu <h2>) 
-//oraz liczbę elementów w kategorii (wszystkie <li>, w nim zagnieżdżone).
-
-const AllCategoryName = ProductById.querySelectorAll("h2");
-
-//console.log(AllCategoryName);
-//console.log(ProductNameByTag);
-
-const CategoryLength = ProductById.querySelectorAll("ul");
-//console.log(CategoryLength);
-
-console.log("Category:"+ AllCategoryName[0].innerHTML);
-console.log("Elements:" + CategoryLength[0].querySelectorAll("li").length);
-
-console.log("Category:"+ AllCategoryName[1].innerHTML);
-console.log("Elements:" + CategoryLength[1].querySelectorAll("li").length);
-
-console.log("Category:"+ AllCategoryName[2].innerHTML);
-console.log("Elements:" + CategoryLength[2].querySelectorAll("li").length);
+productNameByTag.forEach(item => {
+    const categoryName = item.querySelector("h2").innerHTML;
+    const elementsCount = item.querySelectorAll("li").length;
+    
+    console.log("Category: " + categoryName);
+    console.log("Elements: " + elementsCount);
+});
 
 
 
